b78e9f86a8300eaa97aac1512a5b81ee93ddbc3f
Ulviyya Mammadova <113963177+UlviyyaMammadova@users.noreply.github.com>
2024-03-10 21:51:40 +0400

    Fixed the errors

--- test_search.py
+++ test_search.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
@@ -18,11 +19,11 @@ class TestSearch(unittest.TestCase):
         search_field = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "div[data-a-target='tray-search-input'] input")))
 
-
-        search_button = self.driver.find_element(By.CSS_SELECTOR, "button[aria-label='Search Input']")
-
         search_field.send_keys(game_name)
-        search_button.click()
+
+        time.sleep(1)
+
+        search_field.send_keys(Keys.RETURN)
 
         time.sleep(5)
         assert game_name in self.driver.page_source
