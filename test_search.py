1e0197ae0c88112293162509aca364e4b83c1ccf
Ulviyya Mammadova <113963177+UlviyyaMammadova@users.noreply.github.com>
2024-03-10 21:35:40 +0400

    Testing "search function" but encountering with errors

---
+++ test_search.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import unittest
+
+
+class TestSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.get("https://www.twitch.tv")
+
+    def test_search_game(self):
+        game_name = "League of Legends"
+
+        search_field = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "div[data-a-target='tray-search-input'] input")))
+
+
+        search_button = self.driver.find_element(By.CSS_SELECTOR, "button[aria-label='Search Input']")
+
+        search_field.send_keys(game_name)
+        search_button.click()
+
+        time.sleep(5)
+        assert game_name in self.driver.page_source
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
