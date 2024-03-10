9d5c235b1f343ffc19c6414fa32c7101f8e21d18
Ulviyya Mammadova <113963177+UlviyyaMammadova@users.noreply.github.com>
2024-03-10 20:47:05 +0400

    Adding webdriver to Path

--- Test.py
+++ Test.py
@@ -6,16 +6,16 @@ driver = webdriver.Chrome()
 
 driver.get("https://www.alibaba.com/")
 
-search_box = driver.find_element_by_css_selector("input#search-key")
+search_box = driver.find_element_by_css_selector("input[type='text'][placeholder='Search products & suppliers']")
 search_box.send_keys("electronics")
 search_box.send_keys(Keys.RETURN)
 
 time.sleep(3)
 
-search_results = driver.find_elements_by_css_selector("div.mini-product-card")
+search_results = driver.find_elements_by_css_selector("div.product-item")
 assert len(search_results) > 0, "No search results found"
 
 for result in search_results[:5]:
-    print(result.find_element_by_css_selector("h2.title").text)
+    print(result.find_element_by_css_selector("div.title").text)
 
 driver.quit()
