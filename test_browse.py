8211b4c7c7b141ab9081f52f65fe5d4bdac8c7bd
Ulviyya Mammadova <113963177+UlviyyaMammadova@users.noreply.github.com>
2024-03-12 14:57:27 +0400

    Trying to test "browse" function

---
+++ test_browse.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import unittest
+
+class TestBrowseCategory(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
+        self.driver.get("https://www.twitch.tv")
+
+    def test_browse_category(self):
+        browse_link = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "a[data-a-target='side-nav-link']"))
+        )
+        browse_link.click()
+
+        category_tags_input = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-a-target='category-tags-input']"))
+        )
+
+        category_tags_input.send_keys("horror")
+
+        category_tags_input.submit()
+
+        time.sleep(5)
+
+        assert "Horror" in self.driver.page_source
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
